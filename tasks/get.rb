6d30739945f8e3d63e762f704f6014dfae773d09
Lucy Wyman <wyman.lucy@gmail.com>
2019-06-26 14:51:55 -0700

    (BOLT-1408) Add option to be consumed as a Bolt plugin
    This adds an optional boolean parameter `plugin` which defaults to
    false. When true, the output will be formatted as an array of hashes
    which is compatible with the Bolt inventory task plugin.

--- tasks/get.rb
+++ tasks/get.rb
@@ -7,6 +7,7 @@ args = JSON.parse(STDIN.read)
 
 platform = args['platform']
 count = args.fetch('count', 1)
+plugin = args.fetch('plugin', false)
 
 stdout, stderr, status = Open3.capture3('floaty', 'get', "#{platform}=#{count}", '--json')
 
@@ -17,7 +18,8 @@ if status != 0
 end
 
 nodes = Array(JSON.parse(stdout)[platform])
+nodes.map! { |n| { 'uri' => n } } if plugin
 
-result = {nodes: nodes}
+result = { targets: nodes }
 puts result.to_json
 exit 0
